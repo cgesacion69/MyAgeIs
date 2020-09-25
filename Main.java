f66fc062d76a4c603561efe33349024e769885df
cgesacion <cameron.gesacion@patelhs.org>
2020-09-25 12:53:22 +0000

    I inserted my age, I wrote out a code that worked, and I left in hello world to give an introduction

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
+    int myAge= 16;
     System.out.println("Hello world!");
+    System.out.print("I am " + myAge + " years old. ");
   }
 }
